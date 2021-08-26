1d7250ea470e51da5983ee6cca17544c164d5fe1
SL62206 <sagar.lavale@zensar.com>
2021-08-26 22:06:49 +0530

    Eureka Client Configurations

--- src/main/java/com/zensar/master/OlxMasterdataApplication.java
+++ src/main/java/com/zensar/master/OlxMasterdataApplication.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.PathSelectors;
@@ -17,6 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class OlxMasterdataApplication {
 
 	public static void main(String[] args) {
